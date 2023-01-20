832ef0c000eeeae23bcd14efa9def303b08ec17f
izijoe <jospaco2001@yahoo.com>
2023-01-19 16:27:36 -0800

    my monty project

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * f_pint - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fptintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.counter);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
